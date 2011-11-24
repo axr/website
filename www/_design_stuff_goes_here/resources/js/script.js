30685af16c16f055c46716c4b13a4d6aed6b92a1
matzipan <matzipan@gmail.com>
2011-11-24 21:32:04 +0200

    Got rid of modernizr, cleaned the script
    Signed-off-by: matzipan <matzipan@gmail.com>

--- www/_design_stuff_goes_here/resources/js/script.js
+++ www/_design_stuff_goes_here/resources/js/script.js
@@ -1,59 +1,63 @@
 (function($) {
 	$(function(){
-		String.prototype.parseURL = function() {
-			return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/, function(url) {
+		beautifyTweet = function(tweet) {
+		
+			tweet= this.parseURL(tweet);
+			tweet= this.parseUsername(tweet);
+			tweet= this.parseHashtag(tweet);
+			
+			return tweet; 
+		}
+			
+		beautifyTweet.prototype.parseURL = function(tweet) {
+			return tweet.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/, function(url) {
 				return url.link(url);
 			});
 		};
 
-		String.prototype.parseUsername = function() {
-			return this.replace(/[@]+[A-Za-z0-9-_]+/, function(u) {
-				var username = u.replace("@","")
-				return u.link("http://twitter.com/"+username);
+		beautifyTweet.prototype.parseUsername = function(tweet) {
+			return tweet.replace(/[@]+[A-Za-z0-9-_]+/, function(u) {
+				return u.link("http://twitter.com/"+u.replace("@",""));
 			});
 		};
 
-		String.prototype.parseHashtag = function() {
-			return this.replace(/[#]+[A-Za-z0-9-_]+/, function(t) {
-				var tag = t.replace("#","%23")
-				return t.link("http://search.twitter.com/search?q="+tag);
+		beautifyTweet.prototype.parseHashtag = function(tweet) {
+			return tweet.replace(/[#]+[A-Za-z0-9-_]+/, function(t) {
+				return t.link("http://search.twitter.com/search?q="+t.replace("#","%23"));
 			});
 		};
+		
+		$.getJSON("http://api.twitter.com/1/statuses/user_timeline.json?count=1&include_rts=t&screen_name=axrproject&callback=?",
+				function(tweet){
+					$("#container > footer > .activity > .last_tweet > p:first").html(beautifyTweet(tweet[0].text));
+				}
+		);
 	
-		var hash = location.hash;
 		var $manifiesto = $("#manifiesto");
+		var $manifiestoButton = $("#intro .manifiesto");
 		
-		if (hash === "#!/manifiesto") {
-			$manifiesto.slideDown();
-		}
-		
-		var $manifiestoButton = $("#intro .manifiesto").live("click", function() {
+		$manifiestoButton .click(function() {
 			$manifiesto.slideDown();
 		});
 		
-		var $shareOnSocialNetworks = $("#container > #intro > ul.social > li > a").live("click", function(event) {
+		if (location.hash === "#!/manifiesto") {
+			$manifiestoButton.click();
+		}
+		
+		$("#container > #intro > ul.social > li > a").click(function(event) {
 			event.preventDefault();
 			var page = $(this).attr("href");
-			var winH = $(window).height();
-			var winW = $(window).width();
 			var popUpHeight = 245;
 			var popUpWidth = 730;
-			var top = (winH - popUpHeight) / 2;
-			var left = (winW - popUpWidth) / 2;
+			var top = ($(window).height() - popUpHeight) / 2;
+			var left = ($(window).width() - popUpWidth) / 2;
 			var options= "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=yes, width=" + popUpWidth + ", height=" + popUpHeight + ", top= " + top + ", left=" + left;
 			window.open(page,"",options);
 		});
 		
-		var $goToTop = $("#container > footer > a").live("click", function(event){
+		$("#container > footer > a").click(function(event){
 			event.preventDefault();
 			$('html, body').animate({ scrollTop: 0 }, 800);
 		});
-		
-		$.getJSON("http://api.twitter.com/1/statuses/user_timeline.json?count=1&include_rts=t&screen_name=axrproject&callback=?",
-				function(tweet){
-					var $tweet = tweet[0].text.parseURL().parseUsername().parseHashtag();
-					$("#container > footer > .activity > .last_tweet > p:first").html($tweet);
-				}
-		);
 	});
 })(jQuery);
