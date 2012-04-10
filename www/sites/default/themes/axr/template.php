e73a3b0bc4e014071daf30514b34615daac8360d
Ragnis Armus <ragnis.armus@gmail.com>
2012-04-10 23:40:21 +0300

    Use a separate cron file instead of Drupal's cron hook

--- www/sites/default/themes/axr/template.php
+++ www/sites/default/themes/axr/template.php
@@ -1,100 +1,6 @@
 <?php
 
 /**
- * Make sure that the short changelog is available in cache
- *
- * @param string $sha
- */
-function axr_cron_ensure_changelog_short($sha) {
-	$ch = curl_init();
-
-	curl_setopt($ch, CURLOPT_URL,
-		'https://api.github.com/repos/AXR/Prototype/git/tags/'.$sha);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-	$response = curl_exec($ch);
-	curl_close($ch);
-
-	$response = json_decode($response);
-
-	if (!isset($response->object)) {
-		return;
-	}
-
-	$changelog = str_replace("\r", "\n", "\n".$response->message);
-	$changelog = preg_replace("/[\n]+/", "\n", $changelog);
-	$changelog = explode("\nChangelog:\n", $changelog);
-	$changelog = explode("\n", $changelog[1]);
-	$changelog = array_filter($changelog);
-
-	cache_set('axr:changelog_short:'.$sha, serialize($changelog), 'cache',
-		CACHE_PERMANENT);
-}
-
-/**
- * Get list of releases and insert into cache
- */
-function axr_cron_releases_raw() {
-	$ch = curl_init();
-
-	curl_setopt($ch, CURLOPT_URL,
-		'https://api.github.com/repos/AXR/Prototype/git/refs/tags');
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-	$response = curl_exec($ch);
-	curl_close($ch);
-
-	$response = json_decode($response);
-
-	if (!is_array($response))
-	{
-		return;
-	}
-
-	$tags = array();
-
-	for ($i = count($response), $got = 0; $i >= 0; $i--) {
-		if ($got >= 15) {
-			break;
-		}
-
-		if (preg_match('/^refs\/tags\/(v([0-9.]+)\-stable)$/',
-				$response[$i]->ref, $match)) {
-
-			$ch = curl_init();
-
-			curl_setopt($ch, CURLOPT_URL,
-				'https://api.github.com/repos/AXR/Prototype/git/tags/'.
-				$response[$i]->object->sha);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-			$tag = json_decode(curl_exec($ch));
-			curl_close($ch);
-
-			if ($tag === null || $tag === false) {
-				$date = 0;
-			} else {
-				$date = strtotime($tag->tagger->date);
-			}
-
-			$tags[] = (object) array(
-				'version' => $match[2],
-				'tag' => $match[1],
-				'sha' => $response[$i]->object->sha,
-				'date' => $date
-			);
-
-			// Prepare the changelog
-			axr_cron_ensure_changelog_short($response[$i]->object->sha);
-
-			$got++;
-		}
-	}
-
-	cache_set('axr:releases:raw', serialize($tags), 'cache', CACHE_PERMANENT);
-}
-
-/**
  * Check, if release file exists
  *
  * @param string $url
@@ -263,13 +169,6 @@ function axr_breadcrumb($data) {
 	}
 
 	return $html;
-}
-
-/**
- * Drupal cron hook
- */
-function axr_cron() {
-	axr_cron_releases_raw();
 }
 
 /**
