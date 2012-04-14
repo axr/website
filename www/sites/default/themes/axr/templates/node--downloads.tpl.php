874a8a442f65e28a2223cc44cd3b6684e65fa8e8
Ragnis Armus <ragnis.armus@gmail.com>
2012-04-14 16:00:29 +0300

    Implement downloads page. Close #154

---
+++ www/sites/default/themes/axr/templates/node--downloads.tpl.php
@@ -0,0 +1,43 @@
+<div class="downloads">
+	<h2>Prototype</h2>
+
+	<?php
+		$releases = axr_get_releases(0, 5);
+		$arches = array(
+			'x86-64' => '64-bit (x86-64)',
+			'x86' => '32-bit (x64)'
+		);
+	?>
+	<?php if (count($releases) > 0): ?>
+		<ul class="dtable">
+			<li class="head">
+				<span class="version">Version</span>
+				<ul>
+					<li>Windows</li>
+					<li>OS X</li>
+					<li>Linux</li>
+				</ul>
+			</li>
+			<?php foreach ($releases as $release): ?>
+				<li>
+					<span class="version"><?php echo $release->version; ?></span>
+					<ul>
+						<?php foreach (array('win', 'osx', 'linux') as $os): ?>
+							<li>
+								<a href="#"><span class="block_0 <?php echo $os; ?>"></span>download</a>
+								<ul class="archmenu">
+									<?php foreach ($release->urls->$os as $arch => $url): ?>
+										<li><a href="<?php echo $url; ?>"><?php echo isset($arches[$arch]) ? $arches[$arch] : $arch; ?></a></li>
+									<?php endforeach; ?>
+								</ul>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	<?php else: ?>
+		<div class="none">No releases are currently available</div>
+	<?php endif; ?>
+</div>
+
--- www/sites/default/themes/axr/templates/node--home.tpl.php
+++ www/sites/default/themes/axr/templates/node--home.tpl.php
@@ -740,34 +740,3 @@
 	</div>
 </div>
 
-<!--<div class="the_cool_stuff">
-	<div class="nested_0">
-		<div class="downloads">
-			<div class="download buttons">
-				<a class="latest" href="prototype.dmg"><span class="label">download</span><span class="version">0.42 prototype</span></a>
-				<a class="older" href="older_versions.html">older versions</a>
-			</div>
-			<div class="download info">
-				<h2>System Requirements</h2>
-				<p>Right now the prototype requires Mac OS X 10.6 or newer, on a 64bit Intel Mac. Support for Windows and Linux will come soon.</p>
-			</div>
-		</div>
-		<div class="advantages">
-			<h2>Advantages:</h2>
-			<ul>
-				<li><span class="block_0"></span><span>Build websites faster and easier</span></li>
-				<li><span class="block_0"></span><span>Separate content from presentation</span></li>
-				<li><span class="block_0"></span><span>Modularize and reuse your styling objects</span></li>
-				<li><span class="block_0"></span><span>Exactly the same rendering across all browsers</span></li>
-				<li><span class="block_0"></span><span>Vector graphics, powerful effects, textures, etc</span></li>
-				<li><span class="block_0"></span><span>True semantic content</span></li>
-			</ul>
-			<a href="features.html" class="features button_std">
-				<span class="header"></span>
-				<span class="content">See all features<span class="block_0"></span></span>
-				<span class="footer"></span>
-			</a>
-		</div>
-	</div>
-</div>-->
-
