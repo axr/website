8bd4142eaf95789bba6c0bab827cb0e9b556d77d
Ragnis Armus <ragnis.armus@gmail.com>
2012-04-24 16:30:32 +0300

    Fix features menu. Close #198

--- www/sites/default/themes/axr/js/node--bp--home.js
+++ www/sites/default/themes/axr/js/node--bp--home.js
@@ -2,7 +2,9 @@
 	/**
 	 * Handle the HSS Features menu
 	 */
-	$('#hss_features_menu a').click(function () {
+	$('#hss_features_menu a').on('click', function (e) {
+		e.preventDefault();
+
 	    var section_name = $(this).closest('li').attr('data-section-name');
 	    $('#hss_features_menu > .content > li.selected').removeClass('selected');
 	    $('#hss_features_menu > .content > li[data-section-name='+section_name+']').addClass('selected');
