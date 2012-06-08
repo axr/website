5bef42379d4fdf61f1b21a17fc7f3af1b693b009
Ragnis Armus <ragnis.armus@gmail.com>
2012-06-08 16:46:08 +0300

    Use absolute URLS for the menu when not in Drupal

--- www/sites/default/themes/axr/menu.tpl.php
+++ www/sites/default/themes/axr/menu.tpl.php
@@ -1,47 +1,50 @@
+<?php
+	$root = defined('DRUPAL_ROOT') ? '' : 'http://axr.vg';
+?>
 <ul id="menu">
 	<li class="about">
 		<span class="arrow"></span>
-		<a href="/about/features">About</a>
+		<a href="<?php echo $root; ?>/about/features">About</a>
 		<div>
-			<a class="features first" href="/about/features">Features</a>
-			<a class="manifesto last" href="/about/manifesto">Manifesto</a>
-			<!-- <a class="media_kit" href="/under-construction">Media Kit</a> -->
-			<!-- <a class="history" href="/under-construction">History</a> -->
+			<a class="features first" href="<?php echo $root; ?>/about/features">Features</a>
+			<a class="manifesto last" href="<?php echo $root; ?>/about/manifesto">Manifesto</a>
+			<!-- <a class="media_kit" href="<?php echo $root; ?>/under-construction">Media Kit</a> -->
+			<!-- <a class="history" href="<?php echo $root; ?>/under-construction">History</a> -->
 		</div>
 	</li>
 	<li class="specification">
-		<a href="/spec">Specification</a>
+		<a href="<?php echo $root; ?>/spec">Specification</a>
 		</li>
 	<li class="resources">
 		<span class="arrow"></span>
-		<a href="/downloads">Resources</a>
+		<a href="<?php echo $root; ?>/downloads">Resources</a>
 		<div>
-			<a class="downloads first" href="/downloads">Downloads</a>
-			<!-- <a class="examples" href="/under-construction">Examples</a> -->
-			<!-- <a class="tutorials" href="/under-construction">Tutorials</a> -->
+			<a class="downloads first" href="<?php echo $root; ?>/downloads">Downloads</a>
+			<!-- <a class="examples" href="<?php echo $root; ?>/under-construction">Examples</a> -->
+			<!-- <a class="tutorials" href="<?php echo $root; ?>/under-construction">Tutorials</a> -->
 			<a class="documentation last" href="http://docs.axr.vg">Prototype documentation</a>
 		</div>
 	</li>
 	<li class="community">
 		<span class="arrow"></span>
-		<a href="/get-involved">Community</a>
+		<a href="<?php echo $root; ?>/get-involved">Community</a>
 		<div>
-			<a class="get_involved first" href="/get-involved">Get involved</a>
+			<a class="get_involved first" href="<?php echo $root; ?>/get-involved">Get involved</a>
 			<a class="chat" href="http://webchat.freenode.net/?channels=axr" target="_blank">Chat</a>
-			<!-- <a class="forum" href="/under-construction">Forum</a> -->
+			<!-- <a class="forum" href="<?php echo $root; ?>/under-construction">Forum</a> -->
 			<a class="github last" href="https://github.com/AXR" target="_blank">GitHub</a>
 		</div>
 	</li>
 	<li class="wiki">
 		<span class="arrow"></span>
-		<a href="/wiki">Wiki</a>
+		<a href="<?php echo $root; ?>/wiki">Wiki</a>
 		<div>
-		    <a class="main first" href="/wiki">Main page</a>
-			<a class="faq" href="/wiki/FAQ">FAQ</a>
-			<a class="roadmap" href="/wiki/Roadmap">Roadmap</a>
-			<a class="changelog last" href="/wiki/Changelog">Changelog</a>
+		    <a class="main first" href="<?php echo $root; ?>/wiki">Main page</a>
+			<a class="faq" href="<?php echo $root; ?>/wiki/FAQ">FAQ</a>
+			<a class="roadmap" href="<?php echo $root; ?>/wiki/Roadmap">Roadmap</a>
+			<a class="changelog last" href="<?php echo $root; ?>/wiki/Changelog">Changelog</a>
 		</div>
 	</li>
-	<li class="blog"><a href="/blog">Blog</a></li>
+	<li class="blog"><a href="<?php echo $root; ?>/blog">Blog</a></li>
 </ul>
 
