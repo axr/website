97a5282d2d8e365d9b173cb8fe932b4b24e288eb
Ragnis Armus <ragnis.armus@gmail.com>
2012-06-11 00:02:36 +0300

    Ignore config.user.php

--- shared/config.user.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Config::set('/shared/rsrc_url',
-	'http://axrvg.sygise.aragnis.com/sites/default/themes/axr');
-Config::set('/shared/www_url', 'http://axrvg.sygise.aragnis.com');
-Config::set('/shared/wiki_url', 'http://axrvg.sygise.aragnis.com/wiki');
-
