14672462f6dca81862f96bcaeaf4f958f47a18ba
Ragnis Armus <ragnis.armus@gmail.com>
2012-06-26 01:45:50 +0300

    Architecture selection dialog

--- www/sites/default/themes/axr/templates/node--bp--downloads.tpl.php
+++ www/sites/default/themes/axr/templates/node--bp--downloads.tpl.php
@@ -24,17 +24,9 @@
 					<ul>
 						<?php foreach (array('win', 'osx', 'linux') as $os): ?>
 							<li>
-								<a href="#"><span class="block_0 <?php echo $os; ?>"></span>download</a>
-								<ul class="archmenu">
-									<?php $count = 0; ?>
-									<?php foreach ($release->urls->$os as $arch => $url): ?>
-										<?php $count++; ?>
-										<li><a href="<?php echo $url; ?>"><?php echo isset($arches[$arch]) ? $arches[$arch] : $arch; ?></a></li>
-									<?php endforeach; ?>
-									<?php if ($count === 0): ?>
-										<li class="na">Not available</li>
-									<?php endif; ?>
-								</ul>
+								<?php if (count((array) $release->urls->$os) == 0): ?><span>not available</span><?php else: ?>
+									<a href="#" class="dlink" data-version="<?php echo $release->version; ?>" data-os="<?php echo $os; ?>"><span class="block_0 <?php echo $os; ?>"></span>download</a>
+								<?php endif; ?>
 							</li>
 						<?php endforeach; ?>
 					</ul>
@@ -44,5 +36,32 @@
 	<?php else: ?>
 		<div class="none">No releases are currently available</div>
 	<?php endif; ?>
+
+	<script>
+		window.axr_downloads = <?php
+			$data = array();
+
+			if (count($releases) > 0)
+			{
+				foreach ($releases as $release)
+				{
+					$data[$release->version] = $release->urls;
+				}
+			}
+
+			echo json_encode($data);
+		?>;
+	</script>
+</div>
+
+<div id="downloads__ask_arch">
+	<div class="outer">
+		<div class="inner">
+			<a href="#" class="close">☓</a>
+			<h2>You're downloading AXR prototype</h2>
+			<p>You're about to download AXR prototype version <span class="version">unknown</span> for <span class="os">unknown</span>. Please select your system architecture from below:</p>
+			<div class="options"></div>
+		</div>
+	</div>
 </div>
 
