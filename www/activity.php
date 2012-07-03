d77204402ff3874074f50522912434ceca2bf6fb
Ragnis Armus <ragnis.armus@gmail.com>
2012-07-03 22:40:30 +0300

    Include the shared bootstrap

--- www/activity.php
+++ www/activity.php
@@ -9,6 +9,8 @@
 
 define('DRUPAL_ROOT', getcwd());
 
+require_once('../shared/bootstrap.php');
+
 // Bootstrap drupal
 require_once('./includes/bootstrap.inc');
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
--- www/axr_cron.php
+++ www/axr_cron.php
@@ -2,6 +2,8 @@
 
 define('DRUPAL_ROOT', getcwd());
 
+require_once('../shared/bootstrap.php');
+
 // Bootstrap drupal
 require_once('./includes/bootstrap.inc');
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
