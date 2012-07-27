b50fcec89e5caa5be0d06925df85536985f80ab0
Ragnis Armus <ragnis.armus@gmail.com>
2012-07-27 13:05:15 +0300

    Load jQuery before any other scripts

--- www/lib/www_controller.php
+++ www/lib/www_controller.php
@@ -10,9 +10,9 @@ class WWWController extends Controller
 
 		$this->rsrc->loadBundle('css/bundle_shared.css');
 		$this->rsrc->loadBundle('css/bundle_www.css');
-		$this->rsrc->loadBundle('js/bundle_shared.js');
-		$this->rsrc->loadBundle('js/bundle_www.js');
 		$this->rsrc->loadScript('http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js');
+		$this->rsrc->loadBundle('js/bundle_shared.js');
+		$this->rsrc->loadBundle('js/bundle_www.js');	
 	}
 }
 
