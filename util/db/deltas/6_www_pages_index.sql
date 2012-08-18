65c9454f5ae905aaec7c39258abdcd398f73c35e
Ragnis Armus <ragnis.armus@gmail.com>
2012-08-18 14:51:57 +0300

    Delta for www_pages_index table

---
+++ util/db/deltas/6_www_pages_index.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `www_pages_index` (
+  `page_id` int(11) NOT NULL,
+  `field` varchar(255) NOT NULL,
+  `value` mediumtext NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+
