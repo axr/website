9329a850d1c7321c2b4728439b35c12ace04f65b
Ragnis Armus <ragnis.armus@gmail.com>
2012-08-23 19:02:13 +0300

    Remove some more redundant AJAX code

--- www/controllers/get_involved/get_involved.php
+++ www/controllers/get_involved/get_involved.php
@@ -17,26 +17,7 @@ class GetInvolvedController extends WWWController
 			'name' => 'Get involved'
 		);
 
-		if (isset($_GET['_forajax']) && $_GET['_forajax'] === '1')
-		{
-			$this->view->_ajax = true;
-
-			echo json_encode(array(
-				'status' => 0,
-				'payload' => array(
-					'rsrc_bundles' => array(
-						'css/get_involved.css',
-						'js/get_involved.js'
-					),
-					'html' => $this->renderViewOnly(
-						ROOT . '/views/get_involved.html', true)
-				)
-			));
-		}
-		else
-		{
-			echo $this->renderView(ROOT . '/views/get_involved.html');
-		}
+		echo $this->renderView(ROOT . '/views/get_involved.html');
 	}
 }
 
