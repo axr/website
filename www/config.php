12e7631a97597422d7fe767717e597260aee9a31
Ragnis Armus <ragnis.armus@gmail.com>
2012-09-16 19:39:04 +0300

    Show IRC user count. Close #62

--- www/config.php
+++ www/config.php
@@ -9,6 +9,8 @@ Config::set('/www/db/dsn', 'mysql:host=localhost;dbname=test');
 Config::set('/www/db/user', 'root');
 Config::set('/www/db/pass', '');
 
+Config::set('/www/irc_count_file', '/dev/shm/irc_count');
+
 Config::set('/www/ga_account', 'UA-20384487-1');
 
 Config::set('/www/disqus/shortname', 'axr');
--- www/controllers/get_involved/get_involved.php
+++ www/controllers/get_involved/get_involved.php
@@ -11,6 +11,12 @@ class GetInvolvedController extends WWWController
 			'name' => 'Get involved'
 		);
 
+		if (file_exists(Config::get('/www/irc_count_file')))
+		{
+			$this->view->irc_count =
+				file_get_contents(Config::get('/www/irc_count_file'));
+		}
+
 		echo $this->renderView(ROOT . '/views/get_involved.html');
 	}
 }
