b3c6771ab4d6cba7f7c9968eba99d5b184e5a3ce
Ragnis Armus <ragnis.armus@gmail.com>
2012-11-21 19:37:01 +0200

    Don't append ? to URLs when not needed

--- lib/core/url.php
+++ lib/core/url.php
@@ -167,7 +167,7 @@ class URL
 	 */
 	public function to_string ()
 	{
-		return http_build_url('', array(
+		$url = http_build_url('', array(
 			'scheme' => $this->scheme,
 			'host' => $this->host,
 			'user' => $this->user,
@@ -176,6 +176,10 @@ class URL
 			'query' => http_build_query($this->query),
 			'fragment' => $this->fragment
 		));
+
+		$url = preg_replace('/\?$/', '', $url);
+
+		return $url;
 	}
 
 	/**
