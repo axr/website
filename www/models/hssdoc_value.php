ecef3e3489da1599364f896eb8a3d2f1ac072b07
Ragnis Armus <ragnis.armus@gmail.com>
2012-12-21 19:15:20 +0200

    Only run `clear_others_default` when the default flag is set

--- www/models/hssdoc_value.php
+++ www/models/hssdoc_value.php
@@ -22,12 +22,15 @@ class HssdocValue extends \Core\Model
 	 */
 	public function clear_others_default ()
 	{
-		// Don't do this at home
-		\ActiveRecord\Connection::instance('default')->query('
-			UPDATE `www_hssdoc_values` AS `value`
-			SET `value`.`default` = 0
-			WHERE `value`.`property_id` = ' . $this->property_id . ' AND
-				`value`.`id` <> ' . $this->id);
+		if ($this->default)
+		{
+			// Don't do this at home
+			\ActiveRecord\Connection::instance('default')->query('
+				UPDATE `www_hssdoc_values` AS `value`
+				SET `value`.`default` = 0
+				WHERE `value`.`property_id` = ' . $this->property_id . ' AND
+					`value`.`id` <> ' . $this->id);
+		}
 
 		// Do it at a friend's home
 	}
