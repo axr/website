8915b95062c6427f97a9a1324992b5ae91c5b719
veosotano <veosotano@gmail.com>
2013-02-04 20:33:23 +0000

    Update urls to new domain

--- config.php
+++ config.php
@@ -5,20 +5,20 @@ require_once(SHARED . '/lib/core/url.php');
 
 Config::set('/shared/rsrc/prod', true);
 
-Config::set('/shared/rsrc_url', new \URL('http://static.axr.vg/prod-2'));
-Config::set('/shared/www_url', new \URL('http://axr.vg'));
-Config::set('/shared/hssdoc_url', new \URL('http://hss.axr.vg'));
-Config::set('/shared/wiki_url', new \URL('http://wiki.axr.vg'));
+Config::set('/shared/rsrc_url', new \URL('http://static.axrproject.org/prod-2'));
+Config::set('/shared/www_url', new \URL('http://axrproject.org'));
+Config::set('/shared/hssdoc_url', new \URL('http://hss.axrproject.org'));
+Config::set('/shared/wiki_url', new \URL('http://wiki.axrproject.org'));
 
 // When in production, this should be set to the deployet commit's SHA
 Config::set('/shared/version', '0000000');
 
 Config::set('/shared/apps', (object) array(
 	'www' => (object) array(
-		'domains' => array('axr.vg', 'hss.axr.vg')
+		'domains' => array('axrproject.org', 'hss.axrproject.org')
 	),
 	'wiki' => (object) array(
-		'domains' => array('wiki.axr.vg')
+		'domains' => array('wiki.axrproject.org')
 	)
 ));
 
