3c1be3ea15e46eef745ef6ec4a2d1b0c3b940a4c
Ragnis Armus <ragnis.armus@gmail.com>
2013-02-17 13:34:22 +0200

    Clean up config files

--- config.php
+++ config.php
@@ -3,41 +3,32 @@
 require_once(SHARED . '/lib/core/config.php');
 require_once(SHARED . '/lib/core/url.php');
 
-Config::set('/shared/rsrc/prod', true);
+/**
+ * This specified whether the site is running in production or development
+ * environment.
+ */
+Config::set('/shared/prod', true);
 
+/**
+ * Base URLs
+ */
 Config::set('/shared/rsrc_url', new \URL('http://static.axrproject.org/prod-2'));
 Config::set('/shared/www_url', new \URL('http://axrproject.org'));
 Config::set('/shared/hssdoc_url', new \URL('http://hss.axrproject.org'));
 Config::set('/shared/wiki_url', new \URL('http://wiki.axrproject.org'));
 
-// When in production, this should be set to the deployet commit's SHA
+/**
+ * When in production, this should be set to the deployed commit's SHA. In
+ * development environment, this is usually `0000000`
+ */
 Config::set('/shared/version', '0000000');
 
+/**
+ * Memcached servers info
+ */
 Config::set('/shared/cache_servers', array(
 	array('localhost', 11211)
 ));
-
-Config::set('/shared/apps', (object) array(
-	'www' => (object) array(
-		'domains' => array('axrproject.org', 'hss.axrproject.org')
-	),
-	'wiki' => (object) array(
-		'domains' => array('wiki.axrproject.org')
-	)
-));
-
-/**
- * Reccommended development values. You can put them in config.user.php
- *
-
-Config::set('/shared/rsrc/prod', false);
-
-$localhost = 'http://localhost';
-Config::set('/shared/rsrc_url', $localhost . '/static);
-Config::set('/shared/www_url', $localhost;
-Config::set('/shared/wiki_url', $localhost . '/wiki');
-
- */
 
 if (file_exists(dirname(__FILE__) . '/config.user.php'))
 {
