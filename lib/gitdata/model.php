f926664bd49257475d2572935e2f12caa3dace25
Ragnis Armus <ragnis.armus@gmail.com>
2013-04-07 17:59:07 +0300

    Autimatically make h1-h3 titles links

--- lib/gitdata/model.php
+++ lib/gitdata/model.php
@@ -24,6 +24,23 @@ abstract class Model
 			$data = \GitData\Asset::replace_urls_in_html(dirname($file->path), $data);
 		}
 
+		preg_match_all('/<h(?P<n>[1-3])>(?P<title>.+?)<\/h\1>/', $data, $matches);
+
+		for ($i = 0, $c = count($matches['title']); $i < $c; $i++)
+		{
+			$matched = $matches[0][$i];
+			$n = $matches['n'][$i];
+			$title = $matches['title'][$i];
+
+			$alias = strtolower($title);
+			$alias = str_replace(' ', '-', $alias);
+			$alias = preg_replace('/[^a-z0-9-_.]/', '', $alias);
+
+			$replacement = "<h{$n}><a href=\"#{$alias}\" name=\"{$alias}\">{$title}</a></h{$n}>";
+
+			$data = str_replace($matched, $replacement, $data);
+		}
+
 		return $data;
 	}
 
