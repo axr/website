e588d6e304170680837e2a0a1b1955f63f712c9e
Ragnis Armus <ragnis.armus@gmail.com>
2013-04-14 16:21:15 +0300

    Show checksums on downloads page
    Close #458

--- www/www/static/js/www/downloads.js
+++ www/www/static/js/www/downloads.js
@@ -14,6 +14,9 @@
 
 			this._initialized = true;
 
+			$('body').append('<div id="downloads_checksums"></div>');
+			$('body').append('<div id="downloads_checksums_overlay"></div>');
+
 			$('#downloads .rtable > .release > header a.version').on('click', function (e)
 			{
 				e.preventDefault();
@@ -29,9 +32,62 @@
 				});
 			});
 
-			$('#downloads .rtable .pkggroup tbody td.dl a').on('click', function (e)
+			$('#downloads .rtable .pkggroup tbody td.dl a.dl').on('click', function (e)
 			{
 				window._gaq.push(['_trackEvent', 'Downloads', 'DownloadLink', $(this).attr('data-filename')]);
+			});
+
+			$('#downloads_checksums_overlay').on('click', function (e)
+			{
+				$('#downloads_checksums').empty().hide();
+				$('#downloads_checksums_overlay').hide();
+			});
+
+			$('#downloads .rtable .pkggroup tbody td.dl a.checksums').on('click', function (e)
+			{
+				e.preventDefault();
+
+				var popup = $('#downloads_checksums');
+				var checksums = $(this).attr('data-checksums');
+
+				try
+				{
+					checksums = JSON.parse(checksums);
+				}
+				catch (e)
+				{
+					return;
+				}
+
+				popup
+					.css('left', '-9999px')
+					.css('top', '-9999px')
+					.empty()
+					.show();
+
+				var insert_pair = function (name, hash)
+				{
+					popup.append('<dl><dt>{{name}}</dt><dd>{{hash}}</dd></dl>'
+						.replace('{{name}}', name.toUpperCase())
+						.replace('{{hash}}', checksums[name]));
+				};
+
+				// Insert the checksums in a specific order
+				checksums['md5'] && insert_pair('md5', checksums['md5']);
+				checksums['sha1'] && insert_pair('sha1', checksums['sha1']);
+
+				var offset_popup = popup.offset();
+				var offset_link = $(this).offset();
+
+				var x = offset_link.left + (offset_link.width / 2) - (offset_popup.width / 2);
+				var y = offset_link.top - offset_popup.height - 15;
+
+				popup
+					.css('left', x + 'px')
+					.css('top', y + 'px')
+					.show();
+
+				$('#downloads_checksums_overlay').show();
 			});
 		}
 	};
