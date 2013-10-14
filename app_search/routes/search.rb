b72a1a73132752dd5e8760a3d0b0d1f5b35d7208
Ragnis Armus <ragnis.armus@gmail.com>
2013-10-14 18:27:09 +0300

    Simplify search client-side code

--- app_search/routes/search.rb
+++ app_search/routes/search.rb
@@ -6,7 +6,21 @@ module SearchApp
   class App
     get '/q/:query' do
       query = params[:query].gsub('+', ' ')
+      offset = params[:offset].to_i || 0
       simple_query = GitData::Search::Query.simplify(query)
+
+      results = GitData::Search::Query.new(query).results
+      results_html = []
+      result_template = File.read("#{ROOT}/views/result_item.liquid")
+
+      # Take the first 30 results
+      (results.slice(offset, 30) || []).each do |item|
+        item[:item] = item[:item].to_h
+
+        results_html.push Liquid::Template.parse(result_template).render({
+          'item' => item.with_indifferent_access
+        })
+      end
 
       @breadcrumb.append({
         :title => "Results for <strong>#{simple_query}</strong>",
@@ -16,36 +30,11 @@ module SearchApp
       liquid :results, {
         :locals => {
           :query => query,
-          :breadcrumb => breadcrumb(@breadcrumb)
+          :breadcrumb => breadcrumb(@breadcrumb),
+          :results => results_html,
+          :count => (results.count > 30) ? 30 : results.count
         }
       }
-    end
-
-    get '/q.json' do
-      raise Sinatra::NotFound unless params.has_key?('query')
-
-      query = params[:query]
-      offset = params[:offset].to_i || 0
-
-      results = GitData::Search::Query.new(query).results
-
-      html = []
-      template = File.read("#{ROOT}/views/result_item.liquid")
-
-      (results.slice(offset, 10) || []).each do |item|
-        item[:item] = item[:item].to_h
-
-        html.push Liquid::Template.parse(template).render({
-          'item' => item.with_indifferent_access
-        })
-      end
-
-      content_type :json, :charset => 'utf-8'
-      body JSON.generate({
-        :results => html,
-        :has_more => results.length > (offset + 10),
-        :next => (results.length > (offset + 10)) ? offset + 10 : nil
-      })
     end
   end
 end
--- app_search/www/static/js/results.js
+++ app_search/www/static/js/results.js
@@ -1,129 +1,10 @@
 (function ()
 {
-	var LoadAnimation = function (canvas)
-	{
-		const T_ON = 80;
-		const T_OFF = 25;
-
-		var request_frame = (function ()
-		{
-			return window.requestAnimationFrame ||
-				window.webkitRequestAnimationFrame ||
-				window.mozRequestAnimationFrame ||
-				function (callback)
-				{
-					window.setTimeout(callback, 1000 / 60);
-				}
-		})();
-
-		var ctx = $(canvas).get(0).getContext('2d');
-		ctx.canvas.width = $(canvas).width();
-		ctx.canvas.height = $(canvas).height();
-
-		var imagedata = ctx.createImageData(ctx.canvas.width, 1);
-		var pixels = [];
-		var spawn_counter = T_OFF * -1;
-
-		// Initial pixel data
-		{
-			var phase = 1;
-			var phase_i = 0;
-
-			for (var i = 0, c = ctx.canvas.width; i < c; i++)
-			{
-				pixels[i] = phase;
-
-				if (phase === 0 && phase_i >= T_OFF)
-				{
-					phase = 1;
-					phase_i = 0;
-				}
-				else if (phase === 1 && phase_i >= T_ON)
-				{
-					phase = 0;
-					phase_i = 0;
-				}
-
-				phase_i++;
-			}
-		}
-
-		var math_frame = function ()
-		{
-			var end = ctx.canvas.width - 1;
-
-			for (var i = ctx.canvas.width - 1; i >= 0; i--)
-			{
-				if (i === 0)
-				{
-					pixels[0] = 0;
-				}
-				else
-				{
-					pixels[i] = pixels[i - 1];
-				}
-			}
-
-			if (spawn_counter >= 0)
-			{
-				pixels[0] = 1;
-			}
-
-			if (spawn_counter >= T_ON)
-			{
-				spawn_counter = T_OFF * -1;
-			}
-			else
-			{
-				spawn_counter++;
-			}
-		};
-
-		(function animation_loop ()
-		{
-			request_frame(animation_loop);
-
-			for (var i = 0, c = pixels.length; i < c; i++)
-			{
-				imagedata.data[i*4] = 0xD0;
-				imagedata.data[i*4 + 1] = 0x96;
-				imagedata.data[i*4 + 2] = 0x10;
-				imagedata.data[i*4 + 3] = pixels[i] === 1 ? 0xFF : 0x00;
-			}
-
-			for (var y = 0; y < ctx.canvas.height; y++)
-			{
-				ctx.putImageData(imagedata, 0, y);
-			}
-
-			if ($(canvas).width() != ctx.canvas.width ||
-				$(canvas).height() != ctx.canvas.height)
-			{
-				ctx.canvas.width = $(canvas).width();
-				ctx.canvas.height = $(canvas).height();
-
-				pixels = [];
-				imagedata = ctx.createImageData(ctx.canvas.width, 1);
-			}
-		})();
-
-		setTimeout(function math_loop ()
-		{
-			for (var i = 0; i < 5; i++)
-			{
-				math_frame();
-			}
-
-			setTimeout(math_loop, 10);
-		}, 10);
-	};
-
 	var SearchBar = function (element)
 	{
 		var that = this;
 
 		this.element = element;
-		this.rp = null;
 
 		this.sources = {
 			www: {name: 'Website', selected: false},
@@ -225,14 +106,7 @@
 
 		this.submit = function ()
 		{
-			if (that.rp !== null)
-			{
-				Core.Router.instance().navigate('/q/' + encodeURIComponent(that.build_query()));
-			}
-			else
-			{
-				window.location = '/q/' + encodeURIComponent(that.build_query());
-			}
+			window.location = '/q/' + encodeURIComponent(that.build_query());
 		};
 
 		// Load all the source buttons
@@ -288,7 +162,6 @@
 			}
 
 			that.update_options_ui();
-			that.submit();
 		});
 
 		this.element.on('focusin focusout keyup', 'input[name=query]', function (e)
@@ -332,118 +205,5 @@
 		this.clean_query(true);
 	};
 
-	var ResultsPage = function ()
-	{
-		var that = this;
-
-		this.query = null;
-		this.next_offset = 0;
-		this.ajax = [];
-
-		this.set_query = function (query, params)
-		{
-			this.query = query;
-			this.next_offset = 0;
-
-			// Abort all current ajax requests
-			while (this.ajax.length > 0)
-			{
-				this.ajax.pop().abort();
-			}
-
-			this.set_attr('loading', false);
-			this.set_attr('has_more', false);
-			this.set_attr('no_results', false);
-
-			$('#results ._results').empty();
-
-			var query_simple = query.replace(/\b\w+:\w+\b/g, '').replace(/\s+/, ' ');
-			$('#breadcrumb span.current').html('Results for <strong>' + query_simple + '</strong>');
-		};
-
-		this.load_more = function ()
-		{
-			if (this.query === null || this.ajax.length > 0)
-			{
-				return;
-			}
-
-			this.set_attr('loading', true);
-			this.set_attr('has_more', false);
-			this.set_attr('no_results', false);
-
-			this.ajax.push($.ajax({
-				url: '/q.json',
-				data: {
-					query: this.query,
-					offset: this.next_offset
-				},
-				dataType: 'json',
-				success: function (data)
-				{
-					for (var i = 0, c = data.results.length; i < c; i++)
-					{
-						$('#results ._results').append(data.results[i]);
-					}
-
-					Core.CodeBox.find_all($('#results ._results'));
-
-					that.next_offset = data.next;
-					that.ajax.pop();
-
-					that.set_attr('loading', false);
-					that.set_attr('no_results', data.results.length === 0);
-					that.set_attr('has_more', data.has_more);
-				}
-			}));
-		};
-
-		this.set_attr = function (name, value)
-		{
-			var selectors = {
-				loading: '#results .loading',
-				no_results: '#results .no_results',
-				has_more: '#results .load_more',
-			};
-
-			if (selectors[name] !== undefined)
-			{
-				$(selectors[name]).css('display', value ? 'block' : 'none');
-			}
-		};
-
-		new LoadAnimation($('#results .loading canvas'));
-	};
-
-	var rp = new ResultsPage();
-	var sb = new SearchBar($('#search_box'));
-	sb.rp = rp;
-
-	Core.Router.instance().once(/^\/q\//, function ()
-	{
-		$('#results article header .info').tipsy({
-			live: true,
-			gravity: 'e'
-		});
-
-		$('#results .load_more a').on('click', function (e)
-		{
-			e.preventDefault();
-			rp.load_more();
-		});
-	});
-
-	Core.Router.instance().on(/^\/q\//, function ()
-	{
-		var matchdata = Core.Router.instance().url().match(/\/q\/(.+)/) || [];
-		var query = decodeURIComponent(matchdata[1].replace('+', ' '));
-
-		rp.set_query(query);
-		sb.set_query(query);
-
-		if ($('#results ._results').children().length === 0)
-		{
-			rp.load_more();
-		}
-	});
+	new SearchBar($('#search_box'));
 })();
